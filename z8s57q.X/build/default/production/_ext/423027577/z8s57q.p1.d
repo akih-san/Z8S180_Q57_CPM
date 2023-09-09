da18b4f10e423c8366855bc19ae762dab76fc22e
akih-san <112370246+akih-san@users.noreply.github.com>
2023-09-09 17:19:46 +0900

    Add files via upload

---
+++ z8s57q.X/build/default/production/_ext/423027577/z8s57q.p1.d
@@ -0,0 +1,15 @@
+build/default/production/_ext/423027577/z8s57q.p1:  \
+../src/boards/z8s57q.c  \
+../src/boards/../supermez80.h  \
+../src/boards/../../src/picconfig.h  \
+../src/boards/../../fatfs/ff.h  \
+../src/boards/../../fatfs/../fatfs/ffconf.h  \
+../src/boards/../chk_borad_dpend.h  \
+../src/boards/../../drivers/SDCard.h  \
+../src/boards/../../drivers/mcp23s08.h  \
+../src/boards/../../drivers/picregister.h  \
+../src/boards/../../drivers/SPI.h  \
+../src/boards/emuz80_common.c  \
+../src/boards/../../drivers/pic18f47q43_spi.c  \
+../src/boards/../../drivers/SDCard.c  \
+../src/boards/../../drivers/../drivers/utils.h 
---
+++ z8s57q.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=z8s57q.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/z8s57q.X.production.hex
